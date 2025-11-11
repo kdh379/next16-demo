29ecfcdb886a3fb4118f9684590e52c68572e46f
kdh379 <bandal2kim@gmail.com>
2025-11-11 23:06:23 +0900

    init

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import prettierPlugin from "eslint-plugin-prettier";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "auto",
+        },
+      ],
+    },
+  },
+]);
+
+export default eslintConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+import Home from "@/domains/home/home";
+
+export default function HomePage() {
+  return <Home />;
+}
